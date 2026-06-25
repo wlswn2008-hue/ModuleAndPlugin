82c63fa654552c7024b1eb8dc0ee14a155e9d8a5
Jeungha <wlswn2008@gmail.com>
2026-06-25 13:25:31 +0900

    Delete Source directory

--- Source/ModuleAndPlugin/ModuleAndPlugin.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "ModuleAndPlugin.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ModuleAndPlugin, "ModuleAndPlugin" );
- 
--- Source/Test/Test.cpp
+++
@@ -1,6 +0,0 @@
-#include "Test.h"
-#include "Modules/ModuleManager.h"
-
-// 일반(범용) 모듈이므로 별도의 StartupModule/ShutdownModule 구현 없이
-// 엔진이 제공하는 FDefaultModuleImpl을 그대로 사용합니다.
-IMPLEMENT_MODULE(FDefaultModuleImpl, Test);
